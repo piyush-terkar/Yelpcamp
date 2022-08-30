92c4817e12040a727d9d1b40750612e7e48ff553
Piyush Terkar <terkar.piyush3@gmail.com>
2022-08-30 21:36:20 +0530

    db env bug fix

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const reviewRoutes = require("./routes/reviews");
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dburl = "mongodb://localhost:27017/yelp-camp";
-// "mongodb://localhost:27017/yelp-camp" process.env.DB_URL ||
+const dburl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+
 mongoose.connect(dburl, {
   useNewUrlParser: true,
   useCreateIndex: true,
